bd115d5391aa34ecc9c76e53570d3d63299dd8e4
ybhuiyan94 <bhuiyan.yasir@gmail.com>
2019-03-12 17:41:02 -0400

    Sequelize no longer tries to create table on login

--- models/user.js
+++ models/user.js
@@ -45,9 +45,9 @@ User.prototype.validPassword = function (password) {
       }
 
 // create all the defined tables in the specified database.
-sequelize.sync()
-    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-    .catch(error => console.log('This error occured', error));
+// sequelize.sync()
+//     .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+//     .catch(error => console.log('This error occured', error));
 
 // export User model for use in other files.
 module.exports = User;
